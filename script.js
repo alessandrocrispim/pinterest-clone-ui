b892b688a3f27529e989904b432b095568183928
Alessandro Alves Crispim <alessandro.crispim.1979@gmail.com>
2021-09-13 17:51:29 -0300

    termino
    termino

--- script.js
+++ script.js
@@ -1,51 +1,56 @@
-const headings = document.querySelectorAll('.heading .wrapper .text');
-const grids = document.querySelectorAll( '.grid');
-
-console.log(headings);
+const grids = document.querySelectorAll(".grid");
+const headings = document.querySelectorAll(".heading .wrapper .text");
 
 function enterScreen(index) {
-    const grid = grids[index]
-    const heading = headings[index];
-    grid.classList.add('active')
+  const grid = grids[index];
+  const heading = headings[index];
+  const gridColumns = grid.querySelectorAll(".column");
 
+  grid.classList.add("active");
 
-    heading.classList.remove('animate-before', 'animate-after');
+  gridColumns.forEach((element) => {
+    element.classList.remove("animate-before", "animate-after");
+  });
+
+  heading.classList.remove("animate-before", "animate-after");
 }
 
 function exitScreen(index, exitDelay) {
-    const heading = headings[index];
-    const grid = grids [index];
+  const grid = grids[index];
+  const heading = headings[index];
+  const gridColumns = grid.querySelectorAll(".column");
 
-    heading.classList.add('animate-after');
+  gridColumns.forEach((element) => {
+    element.classList.add("animate-after");
+  });
 
-    setTimeout(() => {
-        grid.classList.remove('active');
-    }, exitDelay)
+  heading.classList.add("animate-after");
+
+  setTimeout(() => {
+    grid.classList.remove("active");
+  }, exitDelay);
 }
 
-function setupAnimationCycle({timePerScreen, exitDelay}) {
-    const cycleTime = timePerScreen + exitDelay;
-    let nextIndex = 0;
+function setupAnimationCycle({ timePerScreen, exitDelay }) {
+  const cycleTime = timePerScreen + exitDelay;
+  let nextIndex = 0;
 
-    function nextCycle() {
-       const currentIndex = nextIndex;
+  function nextCycle() {
+    const currentIndex = nextIndex;
 
-       enterScreen(currentIndex);
+    enterScreen(currentIndex);
 
-       setTimeout(() => exitScreen(currentIndex, exitDelay), timePerScreen); 
+    setTimeout(() => exitScreen(currentIndex, exitDelay), timePerScreen);
 
-       nextIndex = nextIndex >= grids.length - 1 ? 0 : nextIndex + 1;
+    nextIndex = nextIndex >= grids.length - 1 ? 0 : nextIndex + 1;
+  }
 
-    }
+  nextCycle();
 
-    nextCycle();
-
-    setInterval(nextCycle, cycleTime);
-
-
+  setInterval(nextCycle, cycleTime);
 }
 
 setupAnimationCycle({
-    timePerScreen: 2000,
-    exitDelay: 200*7
-})
+  timePerScreen: 2000, // ms
+  exitDelay: 200 * 7, // ms
+});
